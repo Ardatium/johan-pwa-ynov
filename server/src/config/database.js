4381191e8df904a0d11db35643e820af0b75dd91
Ardatium <qjohan@lapetitefeuille.com>
2022-12-01 11:06:08 +0100

    Update database.js

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://ynov-student:admin@cluster0.ngcriv2.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ngcriv2.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => console.log("Successfully connected to the database"))
 .catch((error) => console.log(`Error during the connection : ${error}`))
